738137082cd0175e339750297b26301d92187901
jonjau <jonjau3Q@gmail.com>
2020-09-06 23:58:44 +1000

    added spec

---
+++ cribbage.pl
@@ -0,0 +1,99 @@
+card(Card) :-
+    % FIXME: validate cards?
+    true.
+
+change_rep([], []).
+change_rep([card(Rank, Suit) | Cards], Pairs) :-
+    true.
+
+pair(card(Rank, Suit), R-Suit) :-
+    Mapping = [ace-1, jack-11, queen-12, king-13],
+    ( Rank
+    
+    )
+    % ( Rank = ace ->
+    %     R = 1
+    % ; Rank = jack ->
+    %     R = 11
+    % ; Rank = queen ->
+    %     R = 12
+    % ; Rank = king ->
+    %     R = 13
+    % ;
+    %     R = Rank
+    % ).
+
+count_rank([], []).
+count_rank([card(Rank, Suit) | Cards], Counts) :-
+    true.
+
+
+% hand_value(+Hand, +Startcard, ?Value)
+% FIXME: documentation is assessed...
+hand_value(Hand, Startcard, Value) :-
+    Hand = [card(X, Y), card(W, Z)],
+    Startcard = card().
+
+select_hand(Cards, Hand, Cribcards) :-
+    true.
+
+
+
+/*
+tests, and expected output:
+
+?-hand_value([
+    card(7,clubs),
+    card(queen,hearts),
+    card(2,clubs),
+    card(jack,clubs)],
+    card(9,hearts),
+    Value).
+Value = 0.
+
+?-hand_value([
+    card(ace,spades),
+    card(3,hearts),
+    card(king,hearts),
+    card(7,hearts)],
+    card(king,spades),
+    Value).
+Value = 2.
+
+?-hand_value([
+    card(ace,spades),
+    card(3,hearts),
+    card(king,hearts),
+    card(7,hearts)],
+    card(2,diamonds),
+    Value).
+Value = 5.
+
+?-hand_value([
+    card(6,clubs),
+    card(7,clubs),
+    card(8,clubs),
+    card(9,clubs)],
+    card(8,spades),
+    Value).
+Value = 20.
+
+?-hand_value([
+    card(7,hearts),
+    card(9,spades),
+    card(8,clubs),
+    card(7,clubs)],
+    card(8,hearts),
+    Value).
+Value = 24.
+
+?-hand_value([
+    card(5,hearts),
+    card(5,spades),
+    card(5,clubs),
+    card(jack,diamonds)],
+    card(5,diamonds),
+    Value).
+Value = 29.
+
+*/

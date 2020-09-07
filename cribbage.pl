f5a77e164c6bb2875e66fb6061153e0bd2d59f70
jonjau <jonjau3Q@gmail.com>
2020-09-07 16:28:24 +1000

    count_rank working, added factorial and binomial predicates

--- cribbage.pl
+++ cribbage.pl
@@ -2,8 +2,6 @@ card(Card) :-
     % FIXME: validate cards?
     true.
 
-
-
 pair(card(Rank, Suit), R-Suit) :-
     Mappings = [ace-1, jack-11, queen-12, king-13],
     % Suits = [clubs, diamonds, hearts, spades],
@@ -20,11 +18,58 @@ pair2(card(Rank, Suit), R-Suit) :-
 pair2(card(Rank, Suit), Rank-Suit) :-
     integer(Rank), between(2, 10, Rank).
 
+score_pairs([], Acc, Acc).
+score_pairs([Rank-Count | Tail], Acc, Score) :-
+    Count >= 2.
 
-count_rank([], []).
-count_rank([card(Rank, Suit) | Cards], Counts) :-
-    true.
+fact(N, Result) :-
+    fact(N, 1, Result).
+fact(N, A, Result) :-
+    ( N =:= 0 ->
+        Result = A
+    ;
+        N > 0,
+        N1 is N - 1,
+        A1 is A * N,
+        fact(N1, A1, Result)
+    ).
 
+binom(N, K, B) :-
+    fact(N, F1),
+    fact(K, F2),
+    D is N - K,
+    fact(D, F3),
+    B is F1 / (F2 * F3).
+
+
+% TODO: work from the back? to make use of cons
+% count_rank(Tail, [Rank1-Count1, Rank0-Count0 | Tail], Counts),
+% true.
+count_rank([], Rank0-Count0, Acc, [Rank0-Count0 | Acc]).
+count_rank([Rank1-_ | Tail], Rank1-Count0, Acc, Counts) :-
+    Count1 is Count0 + 1,
+    count_rank(Tail, Rank1-Count1, Acc, Counts).
+count_rank([Rank1-_ | Tail], Rank0-Count0, Acc, Counts) :-
+    Rank1 \= Rank0,
+    count_rank(Tail, Rank1-1, [Rank0-Count0 | Acc], Counts).
+
+count_rank(Cards, Counts) :-
+    maplist(pair, Cards, Pairs),
+    msort(Pairs, SortedPairs),
+    SortedPairs = [Rank-_ | Tail],
+    count_rank(Tail, Rank-1, [], Counts).
+
+det(A, B) :-
+    A = 1, B = 2;
+    A = 2, B = 4.
+
+det2(A, B) :-
+    A = 1,
+    B = 2.
+det2(A, B) :-
+    A = 2,
+    B = 4.
+    
 
 % hand_value(+Hand, +Startcard, ?Value)
 % FIXME: documentation is assessed...
